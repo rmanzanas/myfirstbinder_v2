2244e099af351bcf9a00fe18dcb8d7c6d1baa179
Rodrigo Manzanas <rmanzanas@users.noreply.github.com>
2019-08-23 22:33:54 +0200

    Update install.R

--- install.R
+++ install.R
@@ -1,1 +1,4 @@
-
+devtools::install_github("SantanderMetGroup/loadeR")
+devtools::install_github("SantanderMetGroup/transformeR")
+devtools::install_github("SantanderMetGroup/downscaleR")
+devtools::install_github("SantanderMetGroup/visualizeR")
